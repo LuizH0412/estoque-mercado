13b4fd86c237a25b7ff088c46ca5617c855f715d
Luiz henrique <luizpq33@gmail.com>
2024-05-13 00:48:09 -0400

    Criado o models e superuser

--- estoque/admin.py
+++ estoque/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from estoque.models import Produto, Tipo
 
-# Register your models here.
+
+class TipoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome')
+    search_fields = ('id', 'nome')
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'valor', 'data_entrada', 'tipo')
+    search_fields = ('id', 'nome', 'data_entrada')
+
+
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Tipo, TipoAdmin)
+
--- estoque/models.py
+++ estoque/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Tipo(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nome
+
+class Produto(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=200)
+    valor = models.FloatField()
+    data_entrada = models.DateField(auto_now_add=True)
+    tipo = models.ForeignKey(Tipo, on_delete=models.CASCADE, related_name='tipo_produto')
+
+
+    def __str__(self):
+        return self.nome
+    
