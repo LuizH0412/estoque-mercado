2bd1e4ddb6650bf8815376ff7c9f95a83181704d
Luiz henrique <luizpq33@gmail.com>
2024-05-13 21:09:25 -0400

    Signals

--- estoque/apps.py
+++ estoque/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class EstoqueConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'estoque'
+
+    def ready(self):
+        import estoque.signals
--- estoque/signals.py
+++ estoque/signals.py
@@ -3,17 +3,21 @@ from django.db.models import Sum
 from django.dispatch import receiver
 from estoque.models import Produto, ProdutoInventario
 
-@receiver(post_save, sender=Produto)
-def produto_post_save(sender, instance, **kwargs):
+def produto_update_inventario():
     tot_produtos = Produto.objects.all().count()
     tot_valor = Produto.objects.aggregate(
         total_valor=Sum('valor')
     )['total_valor']
     ProdutoInventario.objects.create(
         tot_produtos= tot_produtos,
-        tot_valor= tot_valor,
+        tot_valor= tot_valor
     )
+
+@receiver(post_save, sender=Produto)
+def produto_post_save(sender, instance, **kwargs):
+    produto_update_inventario()
 
 
 @receiver(post_delete, sender=Produto)
 def produto_post_delete(sender, instance, **kwargs):
+    produto_update_inventario()
