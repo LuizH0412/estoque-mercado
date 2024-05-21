e9d11ff7165b5423880c8c47cb46365872649e01
Luiz henrique <luizpq33@gmail.com>
2024-05-20 22:13:28 -0400

    configurando uwsgi_params

--- app/settings.py
+++ app/settings.py
@@ -133,3 +133,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_ROOT = os.path.join(BASEDIR, 'media')
+MEDIA_URL = '/media/'
