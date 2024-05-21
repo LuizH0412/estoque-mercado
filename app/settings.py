b8863e9b948861de3320e196b5f1a03c07dc0c12
Luiz henrique <luizpq33@gmail.com>
2024-05-21 15:18:59 -0400

    .

--- app/settings.py
+++ app/settings.py
@@ -133,7 +133,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
